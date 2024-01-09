b1c5cbf8552c7bb25dba5b8d6c19e939357306ed
ashwithans <ashwithans@gmail.com>
2024-01-09 14:10:10 +0530

    P4PracProj1

---
+++ UsersLoginLib/UserLogin.cs
@@ -0,0 +1,19 @@
+﻿namespace UsersLoginLib
+{
+    public class UserLogin
+    {
+        public string Login(string userName, string userPwd)
+        {
+            string result = "";
+            if (userName == "Ashwitha0905" && userPwd == "Ashu@0905@0905")
+            {
+                result = "Login success";
+            }
+            else
+            {
+                result = "Login Failed";
+            }
+            return result;
+        }
+    }
+}
